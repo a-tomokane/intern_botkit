b6ca5cc5e9cc95368c990746b12a11e5ba3203fc
a-tomokane <a-tomokane@dip-net.co.jp>
2016-09-22 18:13:26 +0900

    レベル2-1追加
    ・ランダム返信

---
+++ level2-1.js
@@ -0,0 +1,32 @@
+if(!process.env.token) {
+    console.log('トークンが設定されていません');
+    process.exit(1);
+}
+
+var Botkit = require('./lib/Botkit.js');
+var os = require('os');
+
+var controller = Botkit.slackbot({
+    debug: true
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+//ランダム返信
+
+controller.hears('(.*)', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    var words = [
+    'こんにちは',
+    'こんばんは',
+    'おはよう',
+    '元気？',
+    'お腹が空いたよ！',
+    'そろそろ眠たくなってきたよ...',
+    '私とのおしゃべりはどうかしら？'
+    ];
+
+    var word = words[Math.floor(Math.random() * words.length)];
+    bot.reply(message, word);
+});
