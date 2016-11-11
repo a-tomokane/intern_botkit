90406ea59b97fb5988e02079760e03e7516aee44
a-tomokane <a-tomokane@dip-net.co.jp>
2016-11-11 21:15:56 +0900

    Cronの実行位置を変更

--- cron.js
+++ cron.js
@@ -12,26 +12,24 @@ var controller = Botkit.slackbot({
 
 var bot = controller.spawn({
     token: process.env.token
-}).startRTM(function(err, bot, playload) {
-    if (err) {
-        throw new Err('Could not connect to Slack.');
-    }
+}).startRTM();
 
-    new CronJob({
-        cronTime: '* * * * *',
-        onTick: function() {
-            bot.say({
-                channel: 'general',
-                text: '@hear: 頑張ってますか？',
-                username: 'kirakira-bot',
-                icon_url: 'https://avatars.slack-edge.com/2016-09-17/80800077655_e9776f26db60dd61c7f5_48.png'
-            }, function(err) {
-                if (err) {
-                    console.log('エラーが発生しました');
-                }
-            });
-        },
-        start: true,
-        timeZone: 'Asia/Tokyo'
-    });
+var cron = new CronJob({
+    cronTime: '*/10 * * * * *',
+    onTick: function() {
+        bot.say({
+            channel: 'general',
+            text: '@hear: 頑張ってますか？',
+            username: 'kirakira-bot',
+            icon_url: 'https://avatars.slack-edge.com/2016-09-17/80800077655_e9776f26db60dd61c7f5_48.png'
+        }, function(err) {
+            if (err) {
+                console.log('エラーが発生しました');
+            }
+        });
+    },
+    start: true,
+    timeZone: 'Asia/Tokyo'
 });
+
+cron.start();
